68797f9e30fb810eb62032eb0062f402f8b68486
hanklee931016 <50302hank@gmail.com>
2025-04-09 13:55:01 +0800

    Add files via upload

---
+++ lab8/script.js
@@ -0,0 +1,35 @@
+var container = document.getElementById('container');
+var counter = 0;
+
+window.onload = function () {
+  container.textContent = add_new_chars(3);
+};
+
+function add_new_chars(x, b = true) {
+  var n = x;
+  if (b) {
+    n = Math.floor(Math.random() * x) + 1;
+  }
+  var str = '';
+  for (let i = 0; i < n; i++) {
+    str += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  }
+  return str;
+}
+
+window.addEventListener("keyup", function (e) {
+  var firstone = container.textContent.substring(0, 1);
+  
+  if (e.key === firstone) {
+    container.textContent = container.textContent.substring(1);
+  } else {
+    
+    if (counter++ >= 2) {
+      container.textContent += add_new_chars(6, false);
+      counter = 0;
+    }
+  }
+
+  
+  container.textContent += add_new_chars(3);
+});
